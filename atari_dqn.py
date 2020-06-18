280a5387e145c1a3f535a14efd17caaf240044a6
Tucker Morgan <tucker.bull.morgan@gmail.com>
2020-06-17 20:36:13 -0700

    Whoops, forgot the atari_dan

--- atari_dqn.py
+++ atari_dqn.py
@@ -1,84 +1,29 @@
-ENV_NAME = 'BreakoutDeterministic-v4'
-LOAD_FROM = './breakout-saves/save-01329871'
-SAVE_PATH = 'breakout-saves'
-LOAD_REPLAY_BUFFER = True
-WRITE_TENSORBOARD = False
-USE_PER = False
-PRIORITY_SCALE = 0.7
-CLIP_REWARD = True
-TOTAL_FRAMES = 30000000
-MAX_EPISODE_LENGTH = 18000
-FRAME_BETWEEN_EVAL = 100000
-EVAL_LENGTH = 10000
-UPDATE_FREQ = 10000
-
-DISCOUNT_FACTOR = 0.99
-MIN_REPLAY_BUFFER_SIZE = 50000
-MEM_SIZE = 1000000
-
-MAX_NOOP_STEPS = 20
-UPDATE_FREQ = 4
-
-INPUT_SHAPE = (84, 84)
-BATCH_SIZE = 32
-LEARNING_RATE = 0.000001
-
-#let us create an actor critic version of this, should not be THAT hard
-def build_v_network(learning_rate=0.00001, input_shape=(84, 84), history_length=4):
-    import tensorflow as tf
-    from tensorflow.keras.initializers import VarianceScaling
-    from tensorflow.keras.layers import (Add, Conv2D, Dense, Flatten, Input, Lambda, Subtract)
-    from tensorflow.keras.models import Model
-    from tensorflow.keras.optimizers import Adam, RMSprop
-
-
-    model_input = Input(shape=(input_shape[0], input_shape[1], history_length)) 
-    x = Lambda(lambda  layer : layer / 255)(model_input)
-    x = Conv2D(32, (8, 8), strides=4, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Conv2D(64, (4, 4), strides=2, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Conv2D(64, (3, 3), strides=1, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Conv2D(1024, (7, 7), strides=1, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Flatten()(x)
-    x = Dense(1, kernel_initializer=VarianceScaling(scale=2.0))(x)
-    model = Model(model_input, x)
-    model.compile(Adam(learning_rate), loss=tf.keras.losses.Huber())
-    return model
-    
-def build_q_network(n_actions, learning_rate=0.00001, input_shape=(84, 84), history_length=4):
-    import tensorflow as tf
-    from tensorflow.keras.initializers import VarianceScaling
-    from tensorflow.keras.layers import (Add, Conv2D, Dense, Flatten, Input, Lambda, Subtract)
-    from tensorflow.keras.models import Model
-    from tensorflow.keras.optimizers import Adam, RMSprop
-
-
-    model_input = Input(shape=(input_shape[0], input_shape[1], history_length))
-    x = Lambda(lambda  layer : layer / 255)(model_input)
-    x = Conv2D(32, (8, 8), strides=4, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Conv2D(64, (4, 4), strides=2, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Conv2D(64, (3, 3), strides=1, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    x = Conv2D(1024, (7, 7), strides=1, kernel_initializer=VarianceScaling(scale=.2), activation='relu', use_bias=False)(x)
-    val_stream, adv_stream = Lambda(lambda w: tf.split(w, 2, 3))(x)
-    
-    val_stream = Flatten()(val_stream)
-    val = Dense(1, kernel_initializer=VarianceScaling(scale=2.0))(val_stream)
-    
-    adv_stream = Flatten()(adv_stream)
-    adv = Dense(n_actions, kernel_initializer=VarianceScaling(scale=2.0))(adv_stream)
-
-    reduce_mean = Lambda(lambda  w: tf.reduce_mean(w, axis=1, keepdims=True))
-    q_vals = Add()([val, Subtract()([adv, reduce_mean(adv)])])
-    model = Model(model_input, q_vals)
-    model.compile(Adam(learning_rate), loss=tf.keras.losses.Huber())
-    return model
-NUMBER_OF_RUNNERS = 1
-networks = []
-target_networks = []
-v_network = []
-num_workers = 0
-
-#class RunnerConfig:
-#    def __init__():
+from multiprocessing import Lock
+class WeightTrainStop:
+    def __init__(self):
+        self.read_counter = 0
+        self.read_lock = Lock()
+        self.write_lock = Lock()
+    def set_weights(self, package):
+        self.write_lock.acquire()
+        (dqn_weights, target_dqn_weights, v_network_weights) = package
+        self.dqn_weights = dqn_weights
+        self.target_dqn_weights = target_dqn_weights
+        self.v_network_weights = v_network_weights
+        self.write_lock.release()
+    def get_weights(self):
+        self.read_lock.acquire()
+        self.read_counter += 1
+        if self.read_counter == 1:
+            self.write_lock.acquire()
+        self.read_lock.release()
+        package = (self.dqn_weights, self.target_dqn_weights, self.v_network_weights)
+        self.read_lock.acquire()
+        self.read_counter -= 1
+        if self.read_counter == 0:
+            self.write_lock.release()
+        self.read_lock.release()
+        return (package)
 
 def controller():
     import cv2
@@ -86,7 +31,9 @@ def controller():
     import tensorflow as tf
     import numpy as numpy
 
+    import CONST
     from multiprocessing import Pool, Queue, Process, cpu_count, Value
+    from utils import build_v_network, build_q_network
     counter = Value("i", 0)
     gpus= tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(gpus[0], True)
@@ -95,81 +42,43 @@ def controller():
     from replay_buffer import ReplayBuffer, Memory
     from learner_agent import LearnerAgent
 
-    INPUT_SHAPE = (84, 84)
-    BATCH_SIZE = 32
-    LEARNING_RATE = 0.000001
-
     runs = 100
     number_of_workers = 4#cpu_count() - 2# - 2 is 1 for learning, 1 is for memory
-    game_wrapper = GameWrapper(ENV_NAME, MAX_NOOP_STEPS)
+    game_wrapper = GameWrapper(CONST.ENV_NAME, CONST.MAX_NOOP_STEPS)
     target_network = build_q_network(game_wrapper.env.action_space.n)
     network = build_q_network(game_wrapper.env.action_space.n)
     v_network = build_v_network()
     per_workers = runs // number_of_workers
-    memory_buffer = ReplayBuffer(size=100000)
+    memory_buffer = ReplayBuffer(size=100000, use_per=True)
     agent = LearnerAgent(network, target_network, v_network, game_wrapper.env.action_space.n)
+    weight_train_station = WeightTrainStop()
+    package = (network.get_weights(), target_network.get_weights(), v_network.get_weights())
+    weight_train_station.set_weights(package)
 
     collected_memories = 0
-    while True:
-        workers = []
-        memory_line = []
-        for i in range(0, number_of_workers):
-            q = Queue()
-            counter.value += 1
-            p = Process(target=runner, \
-                args=(agent.target_dqn.get_weights(), agent.DQN.get_weights(), agent.v_network.get_weights(),per_workers,q, counter) \
-                )
-            p.start()
-            workers.append(p)
-            memory_line.append(q)
-        total_joined = 0
-        workers_done = False
-        while not workers_done:
-            for i in range(0, number_of_workers):
-                if counter.value == 0:
-                        workers_done = True
-                        break
-                try:
-                    memory = memory_line[i].get(block=False)
-                    collected_memories += 1
-                    memory_buffer.add_experience(memory)
-                except Exception as e:
-                    #print(repr(e))
-                    continue
-        for i in range(0, number_of_workers):
-            workers[i].join()
-        if memory_buffer.faux_len() > MIN_REPLAY_BUFFER_SIZE:
-            agent.learn(BATCH_SIZE, gamma, collected_memories)
-            agent.update_target_network()
+    workers = []
+    memory_line = []
+    for i in range(0, number_of_workers):
+        q = Queue()
+        counter.value += 1
+        p = Process(target=runner, args=(per_workers, memory_buffer, weight_train_station))
+        p.start()
+        workers.append(p)
+        memory_line.append(q)
+    lt = Process(target=learner_thread, args=(agent, memory_buffer,weight_train_station))
+    lt.start()
+    for w in workers:
+        w.join()
+    lt.join()
 
-    print(memory_buffer.faux_len())
-
-def memory_thread(from_runner_thread, from_learner_thread, to_learner_thread):
-    from replay_buffer import ReplayBuffer
-    replay_buffer = ReplayBuffer(size=MEM_SIZE)
+def learner_thread(agent, membuffer, weight_train_station):
     while True:
-        try:
-            new_memory = from_learner_thread.get(block=False)
-            replay_buffer.add_experience(new_memory)
-        except Exception as e:
-            noop()
-        try:
-            request = from_learner_thread.get(block=False)
-            if request:
-                mini_batch = replay_buffer.get_minibatch()
-                to_learner_thread.put(mini_batch)
-        except Exception as e:
-            noop()
-
-def learner_thread(agent, to_memory_thread, from_memory_thread):
-    to_memory_thread.put(True)
-    (next_batch, frame_number) = from_memory_thread.get()
-    while True:
-        to_memory_thread.put(True)
+        (next_batch, frame_number) = membuffer.get_minbatch()
         agent.learn(next_batch)
-        (next_batch, frame_number) = from_memory_thread.get()
+        package = (agent.DQN.get_weights(), agent.target_dqn.get_weights(), agent.v_network.get_weights())
+        weight_train_station.add_new_weights(package)
     
-def runner(network_weights, target_network_weights, v_network_weights, runs, q, counter):
+def runner(runs, memory_buffer, weight_train_stop):
     import cv2
     import time
     import tensorflow as tf
@@ -185,33 +94,38 @@ def runner(network_weights, target_network_weights, v_network_weights, runs, q, 
     from tensorflow.keras.layers import (Add, Conv2D, Dense, Flatten, Input, Lambda, Subtract)
     from tensorflow.keras.models import Model
     from tensorflow.keras.optimizers import Adam, RMSprop
+    from utils import build_q_network, build_v_network
+    import CONST
 
     gpus= tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(gpus[0], True)
 
-    INPUT_SHAPE = (84, 84)
-    BATCH_SIZE = 32
-    LEARNING_RATE = 0.000001
-    game_wrapper = GameWrapper(ENV_NAME, MAX_NOOP_STEPS)
+    game_wrapper = GameWrapper(CONST.ENV_NAME, CONST.MAX_NOOP_STEPS)
+
     target_network = build_q_network(game_wrapper.env.action_space.n)
     network = build_q_network(game_wrapper.env.action_space.n)
     v_network = build_v_network()
-    network.set_weights(network_weights)
-    target_network.set_weights(target_network_weights)
-    v_network.set_weights(v_network_weights)
-    agent = RunnerAgent(network, target_network, v_network, 4, input_shape=INPUT_SHAPE)
-    for i in range(0, runs):
-        print(i)
+
+    agent = RunnerAgent(network, target_network, v_network, 4, input_shape=CONST.INPUT_SHAPE)
+    generation = 0
+    while True:
+        print("Starting genreation: ", generation)
+        (dqn_weight, target_dqn_weights, v_network) = weight_train_stop.get_weights()
+        agent.DQN.set_weights(dqn_weight)
+        agent.target_dqn.set_weights(target_dqn_weights)
+        agent.v_network.set_weights(v_network)
         frame_number = 0
-        terminal = False
-        game_wrapper.reset()
-        while not terminal:
-            action = agent.get_action(frame_number, game_wrapper.state)
-            processed_frame, reward, terminal, life_lost = game_wrapper.step(action)
-            a_memory = Memory(action, processed_frame[:,:, 0], reward, terminal, True)
-            q.put(a_memory)
-            frame_number+=1
-    counter.value -= 1
+        for i in range(0, runs):
+            terminal = False
+            game_wrapper.reset()
+            while not terminal:
+                action = agent.get_action(frame_number, game_wrapper.state)
+                processed_frame, reward, terminal, life_lost = game_wrapper.step(action)
+                a_memory = Memory(action, processed_frame[:,:, 0], reward, terminal, True)
+                memory_buffer.add_experience(a_memory)
+                frame_number+=1
+        generation += 1
+    
 
 if __name__ == '__main__':
     from multiprocessing import Pool, Queue, Process
